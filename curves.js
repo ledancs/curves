43b9f1a7e84789b68b40e946ca0556b683ffcd66
ledancs <andres.ledesma@tut.fi>
2015-05-23 01:06:32 +0300

    Draggable curves and other refactoring.

--- curves.js
+++ curves.js
@@ -2,47 +2,204 @@
  * Created by ledesmaf on 22.5.2015.
  */
 
+
 function Curves(w, h, groupedMeasurements, className){
+
+    /**
+     *
+     * @param groupedMeasurements
+     */
+    function getMeasurements(groupedMeasurements){
+
+        var collection = [];
+
+        var group, measurements;
+
+        for(var i = 0; i < groupedMeasurements.length; i ++){
+            group = groupedMeasurements[i];
+            measurements = group.measurements;
+            collection = collection.concat(measurements);
+        }
+
+        return collection;
+    }
+
+    /**
+     *
+     * @param measurements
+     * @returns {{min: number, max: number}}
+     */
+    function getMinMax(measurements){
+        var measurement, samples, sample, timestamp;
+        var timestamps = [];
+        for(var i = 0; i < measurements.length; i ++){
+            measurement = measurements[i];
+            samples = measurement.samples;
+            for(var j = 0; j < samples.length; j ++){
+                sample = samples[j];
+                timestamp = sample.timestamp;
+                if(timestamps.indexOf(timestamp) < 0) // only add unique elements
+                    timestamps.push(timestamp);
+            }
+        }
+
+        var min = Math.min.apply(null, timestamps);
+        var max = Math.max.apply(null, timestamps);
+
+        return {
+            min: min,
+            max: max
+        };
+    }
+
+    /**
+     *
+     * @param measurement
+     * @param value
+     * @returns {string}
+     */
+    function getColor(measurement, value){
+        var optionalRanges = ["yellow_min", "yellow_max", "red_min", "red_max"];
+        var ranges = {};
+        var hasRanges = false;
+        var color = "white";
+        var rangeName = "";
+
+        for(var i = 0; i < optionalRanges.length; i++){
+            rangeName = optionalRanges[i];
+            if (typeof measurement[rangeName] != 'undefined'){
+                hasRanges = true; // set the bool true
+                ranges[rangeName] = measurement[rangeName]; // add to the object
+            }
+        }
+
+        if(!hasRanges)
+            return color; // no ranges so return white
+
+        color = "#73D651"; // set our nice green color
+
+        if(ranges["yellow_max"] && value >= measurement.max){
+            color = "gold";
+        }
+        if(ranges["red_max"] && value >= ranges["red_max"]){
+            color = "tomato";
+        }
+
+        if(ranges["yellow_min"] && value <= measurement.min){
+            color = "gold";
+        }
+        if(ranges["red_min"] && value <= ranges["red_min"]){
+            color = "tomato";
+        }
+
+        return color;
+    }
+
+    /**
+     *
+     * @param measurement
+     * @returns {Array}
+     */
+    function getLinePoints(measurement){
+        var points = [];
+        var samples = measurement.samples;
+        var sample1, sample2;
+        var linePoints;
+        for(var i = 0; i + 1 < samples.length; i ++){
+            sample1 = samples[i];
+            sample2 = samples[i + 1];
+            linePoints = {
+                timestamp1: sample1.timestamp,
+                timestamp2: sample2.timestamp,
+                value1: sample1.value,
+                value2: sample2.value
+            };
+            points.push(linePoints);
+        }
+
+        return points;
+    }
+
+    var dx = 0;
+    var drag = d3.behavior.drag()
+        .on("drag", function(d, i) {
+            dx += d3.event.dx; // add the movement in x
+            console.log(d3.event.dx);
+            // negative means we move it to the future
+            // positive we mov it to the past
+
+            // set a limit
+            // limits change with the window size
+
+            if(dx < w - limitX)
+                dx = w - limitX;
+            if(dx > window.x1)
+                dx = window.x1;
+            // we need to do this for all the curve elements
+            // transform translate the group
+            container.selectAll("g.draggable")
+                .attr("transform", function(d, i){
+                    return "translate(" + dx + ", 0)";
+                })
+        });
+
     var div = d3.select("body")
         .append("div")
         .attr("class", className);
 
     var svg = div.append("svg")
-        .attr("width", w + 10)
+        .attr("width", w + 2)
         .attr("height", h + 10); // some margin
 
     var y = 30;
+    var offset = 60;
+    var frameHeight = 50;
+    var hMeasurements = getMeasurements(groupedMeasurements);
+    var minMax = getMinMax(hMeasurements);
+    var timeMin = minMax.min;
+    var timeMax = minMax.max;
+    var window = {
+        timestamp1: timeMin - (24 * 60 * 60),
+        timestamp2: timeMin + (31 * 24 * 60 * 60), // a month window
+        x1: 0, // to be updated with the timeScale
+        x2: 0
+    }
 
-    var hMeasurements = new HealthMeasurements(groupedMeasurements);
-
+    // time scale for the given window
     var timeScale = d3.scale.linear()
-        .domain([hMeasurements.min - (1 * 24 * 60 * 60), hMeasurements.max + (1 * 24 * 60 * 60)])
+        .domain([window.timestamp1, window.timestamp2])
         .range([0, w]);
-
+    window.x1 = timeScale(window.timestamp1);
+    window.x2 = timeScale(window.timestamp2);
+    var limitX = timeScale(timeMax + (24 * 60 * 60));
     var container = svg.append("g").attr("class", "container");
 
-    var m, valueScale, g;
-    for(var i = 0; i < hMeasurements.collection.length; i ++){
+    var m, valueScale, g, draggable;
+    for(var i = 0; i < hMeasurements.length; i ++){
 
-        m = hMeasurements.collection[i]; // for testing
+        // extract the measurement
+        m = hMeasurements[i];
 
-
+        // the value scale for this curve
         valueScale = d3.scale.linear()
             .domain([m.min, m.max])
             .range([0, h/2]);
 
+        // curve main container
         g = container.append("g")
             .attr("class", "curve");
 
+        // label text
         g.append("text")
             .attr({
-                "x": 0,
-                "y": y - 15,
+                "x": 2,
+                "y": y - offset/4,
                 "font-size": 14,
                 "fill": "grey"
             })
             .text(m.label);
 
+        // wellness zone
         g.append("rect")
             .attr({
                 "x": 0,
@@ -51,16 +208,45 @@ function Curves(w, h, groupedMeasurements, className){
                 "height": h/2,
                 "stroke": "none",
                 "fill": "green",
-                "opacity": 0.15
+                "opacity": 0.15,
+                "class": "wellness-zone"
             });
 
+        // draggable group
+        draggable = g.append("g")
+            .attr("class", "draggable")
+            .data([{
+                "x": 0
+            }])
+            .call(drag);
 
-        /*
-        TODO:
-            Build viewModels for the lines and the circles.
-         */
+        // lines
+        draggable.append("g")
+            .attr("class", "lines")
+            .selectAll("line")
+            .data(getLinePoints(m))
+            .enter()
+            .append("line")
+            .attr("x1", function(d){
+                return timeScale(d.timestamp1);
+            })
+            .attr("x2", function (d) {
+                return timeScale(d.timestamp2);
+            })
+            .attr("y1", function (d) {
+                return y + h/2 + h/4 - valueScale(d.value1);
+            })
+            .attr("y2", function (d) {
+                return y + h/2 + h/4 - valueScale(d.value2);
+            })
+            .attr({
+                "stroke-width": 1,
+                "stroke": "grey",
+                "vector-effect": "non-scaling-stroke"
+            });
 
-        g.append("g")
+        // circles
+        draggable.append("g")
             .attr("class", "measurements")
             .selectAll("circle")
             .data(m.samples)
@@ -73,50 +259,53 @@ function Curves(w, h, groupedMeasurements, className){
                 return y + h/2 + h/4 - valueScale(d.value);
             })
             .attr("fill", function (d) {
-                return d.value > m.max || d.value < m.min ? "tomato": "limegreen";
+                // clever function comes here
+                return getColor(m, d.value);
             })
             .attr({
+                "stroke-width": 2,
+                "r": 4,
                 "stroke": "grey",
-                "stroke-width": 1,
-                "r": 4
+                "vector-effect": "non-scaling-stroke"
             });
-        y += h + 60;
+
+
+        // draggable rectangle
+        draggable.append("rect")
+             .attr({
+                 "x": 0,
+                 "y": y - offset/2,
+                 "height": h + frameHeight,
+                 "fill": "white",
+                 "opacity": 0,
+                 "stroke": "none",
+                 "class": "mask"
+             })
+            .attr("width", function(d){
+                 return timeScale(timeMax + (24 * 60 * 60));
+             })
+
+        // Frame
+        g.append("rect")
+            .attr({
+                "x": 0,
+                "y": y - offset/2,
+                "height": h + frameHeight,
+                "width": w,
+                "fill": "none",
+                "stroke": "grey",
+                "stroke-width": 1.25,
+                "vector-effect": "non-scaling-stroke",
+                "class": "frame"
+            });
+        // update the offset
+        y += h + offset;
     }
 
 
 
-    var translate = "translate(" + 5 + ", " + 5 + ")";
+    var translate = "translate(" + 1 + ", " + 5 + ")";
     container.attr("transform", translate);
 
     svg.attr("height", y + 5);
-}
-
-function HealthMeasurements(groupedMeasurements){
-
-    this.collection = [];
-
-    var group, measurements, measurement, samples, sample, timestamp;
-    var timestamps = [];
-
-    for(var i = 0; i < groupedMeasurements.length; i ++){
-        group = groupedMeasurements[i];
-        measurements = group.measurements;
-        this.collection = this.collection.concat(measurements);
-    }
-
-
-    for(var i = 0; i < this.collection.length; i ++){
-        measurement = this.collection[i];
-        samples = measurement.samples;
-        for(var j = 0; j < samples.length; j ++){
-            sample = samples[j];
-            timestamp = sample.timestamp;
-            if(timestamps.indexOf(timestamp) < 0)
-                timestamps.push(timestamp);
-        }
-    }
-
-    this.min = Math.min.apply(null, timestamps);
-    this.max = Math.max.apply(null, timestamps);
-
 }
